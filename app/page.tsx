ba4d490efa22dd9e1885541cd65540691bc1e5e0
Sean Swanson <sean-swan@hotmail.com>
2023-05-20 14:09:24 -0700

    Fixed escape apos

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center gap-6">
       <Image src="swansong-thick-svg.svg" width={100} height={100} />
-      <h1>Hey there! We're cooking something up.</h1>
+      <h1>Hey there! We&apos;re cooking something up.</h1>
       <p>
         Inquiries can be sent{" "}
         <a className="text-blue-600" href="mailto:hello@swansondigitalarts.com">
